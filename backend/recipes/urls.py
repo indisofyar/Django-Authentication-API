f620cdc0e20828061cbf2aa4ea2579c9892ef3db
indisofyar <38954340+indisofyar@users.noreply.github.com>
2024-11-12 11:05:50 +0000

    Progressed recipe app

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from recipes.views import RecipeList
 
-from .views import search_google
+from .views import search_google, extract_recipe, recipe, delete_recipe
 
 urlpatterns = [
+    path('recipe/<int:recipe_id>', recipe, name='recipe'),
     path('recipes/', RecipeList.as_view(), name='recipe-list'),
+    path('delete/', delete_recipe),
+    path('extract-recipe/', extract_recipe),
     path('search-google/<str:search_term>/', search_google, ),
 ]
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,18 +1,23 @@
 import os
-import requests
 
-from django.shortcuts import render
+import time
+
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 
-from .models import Recipe
+from .models import Recipe, Ingredient, Step
 from .serializers import RecipeSerializer
 
 
 class RecipeList(ListAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+
+@api_view(['GET'])
+def recipe(request, recipe_id):
+    return Response(RecipeSerializer(Recipe.objects.get(id=recipe_id)).data)
 
 
 @api_view(['GET'])
@@ -23,6 +28,291 @@ def search_google(request, search_term):
     response = requests.get(url)
     print(response.json())
     return Response(response.json())
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from bs4 import BeautifulSoup
+import requests
+import time
+from .models import Recipe  # Import the Recipe model
+import numpy as np
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from bs4 import BeautifulSoup
+import requests
+import time
+import numpy as np
+
+
+def extract_bbc_user_recipe(html, url, image_url):
+    soup = BeautifulSoup(html.text, 'html.parser')
+    try:
+        recipe_title = soup.find('h1', {'class': 'heading-1'}).text
+    except:
+        recipe_title = np.nan
+    try:
+        difficulty = soup.find_all('div', {'class': 'icon-with-text__children'})[1].text
+    except:
+        difficulty = np.nan
+    try:
+        serves = soup.find_all('div', {'class': 'icon-with-text__children'})[2].text
+    except:
+        serves = np.nan
+    try:
+        rating = soup.find_all('span', {'class': 'sr-only'})[26].text
+    except:
+        rating = np.nan
+    try:
+        number_of_review = soup.find('span', {'class': 'rating__count-text body-copy-small'}).text
+    except:
+        number_of_review = np.nan
+    try:
+        prep_time = soup.find('li', {'class': 'body-copy-small list-item'}).text
+    except:
+        prep_time = np.nan
+    try:
+        cook_time = soup.find_all('li', {'class': 'body-copy-small list-item'})[1].text
+    except:
+        cook_time = np.nan
+    try:
+        categories = soup.find_all('ul', {
+            'class': 'terms-icons-list d-flex post-header__term-icons-list mt-sm hidden-print list list--horizontal'})[
+            0].text
+        vegetarian = 'Vegetarian' in categories
+        vegan = 'Vegan' in categories
+        keto = 'Keto' in categories
+        dairy_free = 'Dairy-free' in categories
+        gluten_free = 'Gluten-free' in categories
+    except:
+        vegetarian = False
+        vegan = False
+        keto = False
+        dairy_free = False
+        gluten_free = False
+
+    # Collect ingredients as objects in a list instead of directly assigning them to the recipe
+    ingredient_list = []
+    ingredients = soup.find_all('li', {'class': 'pb-xxs pt-xxs list-item list-item--separator'})
+    ingredients += soup.find_all('li', {'class': 'ingredients-list__item list-item list-item--separator-top'})
+
+    for ingredient in ingredients:
+        try:
+            ingredient_string = ''.join(str(ingredient).split('<!-- -->')[1])
+        except Exception as e:
+            ingredient_string = ''.join(ingredient.text)
+
+        ingredient_obj = Ingredient.objects.create(text=ingredient_string.replace('</li>', ''))
+        ingredient_list.append(ingredient_obj)
+
+    print(f'Loaded recipe: {recipe_title}')
+
+    # Collect steps as objects in a list
+    steps_list = []
+    steps = soup.find_all('li', {'class': 'pb-xs pt-xs list-item'})
+
+    for i, step in enumerate(steps):
+        # Directly access the <p> element within the step
+        text = step.find('div', {'class': 'editor-content'})
+
+        # Check if <p> element exists before proceeding
+        if text:
+            step_obj = Step.objects.create(text=text.get_text(strip=True), order=i)
+            steps_list.append(step_obj)
+
+    recipe_data = {
+        'title': recipe_title,
+        'difficulty': difficulty,
+        'serves': serves,
+        'rating': rating,
+        'reviews': number_of_review,
+        'vegetarian': vegetarian,
+        'vegan': vegan,
+        'keto': keto,
+        'dairy_free': dairy_free,
+        'gluten_free': gluten_free,
+        'prep_time': prep_time,
+        'cook_time': cook_time,
+        'ingredients': ingredient_list,
+    }
+
+    recipe = Recipe.objects.filter(title=recipe_title)
+
+    # Save to the database
+    if recipe_title and not Recipe.objects.filter(title=recipe_title).exists():
+
+        recipe = Recipe.objects.create(
+            title=recipe_data['title'],
+            difficulty=recipe_data.get('difficulty'),
+            serves=recipe_data.get('serves'),
+            rating=recipe_data.get('rating'),
+            reviews=recipe_data.get('reviews'),
+            vegetarian=recipe_data.get('vegetarian'),
+            vegan=recipe_data.get('vegan'),
+            keto=recipe_data.get('keto'),
+            dairy_free=recipe_data.get('dairy_free'),
+            gluten_free=recipe_data.get('gluten_free'),
+            prep_time=recipe_data.get('prep_time'),
+            cook_time=recipe_data.get('cook_time'),
+            recipe_image_url=image_url,
+            recipe_url=url,
+        )
+
+        # Use `.set()` to assign ingredients to the many-to-many field
+        recipe.ingredients.set(ingredient_list)
+        recipe.steps.set(steps_list)
+        recipe.save()
+        print(f"Recipe '{recipe.title}' saved to the database.")
+    else:
+        recipe = recipe.first()
+        print(f"Recipe '{recipe_title}' already exists in the database.")
+
+    return recipe
+
+
+def extract_bbc_curator_recipe(html, url, image_url):
+    soup = BeautifulSoup(html.text, 'html.parser')
+    try:
+        recipe_title = soup.find('h1', {'class': 'heading-1'}).text
+    except:
+        recipe_title = np.nan
+    try:
+        difficulty = soup.find_all('div', {'class': 'icon-with-text__children'})[1].text
+    except:
+        difficulty = np.nan
+    try:
+        serves = soup.find_all('div', {'class': 'icon-with-text__children'})[2].text
+    except:
+        serves = np.nan
+    try:
+        rating = soup.find_all('span', {'class': 'sr-only'})[26].text
+    except:
+        rating = np.nan
+    try:
+        number_of_review = soup.find('span', {'class': 'rating__count-text body-copy-small'}).text
+    except:
+        number_of_review = np.nan
+    try:
+        prep_time = soup.find('li', {'class': 'body-copy-small list-item'}).text
+    except:
+        prep_time = np.nan
+    try:
+        cook_time = soup.find_all('li', {'class': 'body-copy-small list-item'})[1].text
+    except:
+        cook_time = np.nan
+    try:
+        categories = soup.find_all('ul', {
+            'class': 'terms-icons-list d-flex post-header__term-icons-list mt-sm hidden-print list list--horizontal'})[
+            0].text
+        vegetarian = 'Vegetarian' in categories
+        vegan = 'Vegan' in categories
+        keto = 'Keto' in categories
+        dairy_free = 'Dairy-free' in categories
+        gluten_free = 'Gluten-free' in categories
+    except:
+        vegetarian = False
+        vegan = False
+        keto = False
+        dairy_free = False
+        gluten_free = False
+
+    # Collect ingredients as objects in a list instead of directly assigning them to the recipe
+    ingredient_list = []
+    ingredients = soup.find_all('li', {'class': 'ingredients-list__item list-item'})
+    ingredients += soup.find_all('li', {'class': 'ingredients-list__item list-item list-item--separator-top'})
+
+    for ingredient in ingredients:
+        try:
+            ingredient_string = ''.join(str(ingredient).split('<!-- -->')[1])
+        except Exception as e:
+            ingredient_string = ''.join(ingredient.text)
+
+        ingredient_obj = Ingredient.objects.create(text=ingredient_string.replace('</li>', ''))
+        ingredient_list.append(ingredient_obj)
+
+    print(f'Loaded recipe: {recipe_title}')
+
+    # Collect steps as objects in a list
+    steps_list = []
+    steps = soup.find_all('li', {'class': 'method-steps__list-item'})
+
+    for i, step in enumerate(steps):
+        # Directly access the <p> element within the step
+        text = step.find('p')
+
+        # Check if <p> element exists before proceeding
+        if text:
+            step_obj = Step.objects.create(text=text.get_text(strip=False), order=i)
+            steps_list.append(step_obj)
+
+    recipe_data = {
+        'title': recipe_title,
+        'difficulty': difficulty,
+        'serves': serves,
+        'rating': rating,
+        'reviews': number_of_review,
+        'vegetarian': vegetarian,
+        'vegan': vegan,
+        'keto': keto,
+        'dairy_free': dairy_free,
+        'gluten_free': gluten_free,
+        'prep_time': prep_time,
+        'cook_time': cook_time,
+        'ingredients': ingredient_list,
+    }
+
+    recipe = Recipe.objects.filter(title=recipe_title)
+
+    # Save to the database
+    if recipe_title and not Recipe.objects.filter(title=recipe_title).exists():
+
+        recipe = Recipe.objects.create(
+            title=recipe_data['title'],
+            difficulty=recipe_data.get('difficulty'),
+            serves=recipe_data.get('serves'),
+            rating=recipe_data.get('rating'),
+            reviews=recipe_data.get('reviews'),
+            vegetarian=recipe_data.get('vegetarian'),
+            vegan=recipe_data.get('vegan'),
+            keto=recipe_data.get('keto'),
+            dairy_free=recipe_data.get('dairy_free'),
+            gluten_free=recipe_data.get('gluten_free'),
+            prep_time=recipe_data.get('prep_time'),
+            cook_time=recipe_data.get('cook_time'),
+            recipe_image_url=image_url,
+            recipe_url=url,
+        )
+
+        # Use `.set()` to assign ingredients to the many-to-many field
+        recipe.ingredients.set(ingredient_list)
+        recipe.steps.set(steps_list)
+        recipe.save()
+        print(f"Recipe '{recipe.title}' saved to the database.")
+    else:
+        recipe = recipe.first()
+        print(f"Recipe '{recipe_title}' already exists in the database.")
+
+    return recipe
+
+
+@api_view(['POST'])
+def extract_recipe(request):
+    recipe_data = None
+
+    sleep_timer = 1
+    time.sleep(sleep_timer)
+    url = request.data['url']
+
+    image_url = request.data['image_url'] if 'image_url' in request.data else None
+    html = requests.get(url)
+
+    if 'bbcgoodfood.com/user/' in url:
+        recipe = extract_bbc_user_recipe(html=html, url=url, image_url=image_url)
+    elif 'bbcgoodfood' in url:
+        recipe = extract_bbc_curator_recipe(html=html, url=url, image_url=image_url)
+
+    return Response(RecipeSerializer(recipe).data)
 
 
 import datetime
@@ -41,137 +331,7 @@ def range_of_numbers(n):
     return list(range(1, n + 1))
 
 
-def extract(pages, sleep_timer):
-    def get_urls():
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
-        urls_df = pd.DataFrame(columns=['recipe_urls'])
-
-        for page in pages:
-            time.sleep(sleep_timer)
-            url = f'https://www.bbcgoodfood.com/search/recipes/page/{page}/?sort=-popular&meal-type=dinner'
-            html = requests.get(url)
-            soup = BeautifulSoup(html.text, 'html.parser')
-            recipe_urls = pd.Series([a.get("href") for a in soup.find_all("a")])
-            recipe_urls = recipe_urls[(recipe_urls.str.count("-") > 0)
-                                      & (recipe_urls.str.contains("/recipes/") == True)
-                                      & (recipe_urls.str.contains("category") == False)
-                                      & (recipe_urls.str.contains("collection") == False)].unique()
-            df = pd.DataFrame({"recipe_urls": recipe_urls})
-            urls_df = pd.concat([urls_df, df], ignore_index=True)
-
-        urls_df['recipe_urls'] = 'https://www.bbcgoodfood.com' + urls_df['recipe_urls'].astype(str)
-        recipes_df = pd.DataFrame(
-            columns=['title', 'difficulty', 'serves', 'rating', 'reviews', 'vegetarian', 'vegan', 'dairy_free', 'keto',
-                     'gluten_free', 'prep_time', 'cook_time', 'ingredients'])
-        list_urls = urls_df['recipe_urls'].to_list()
-        return list_urls, urls_df, recipes_df
-
-    def get_recipes(list_urls, urls_df, recipes_df):
-
-        for i in range(len(list_urls)):
-            time.sleep(sleep_timer)
-            url = list_urls[i]
-            html = requests.get(url)
-            soup = BeautifulSoup(html.text, 'html.parser')
-
-            try:
-                recipe_title = soup.find('h1', {'class': 'heading-1'}).text
-            except:
-                recipe_title = np.nan
-            try:
-                difficulty = soup.find_all('div', {'class': 'icon-with-text__children'})[1].text
-            except:
-                difficulty = np.nan
-            try:
-                serves = soup.find_all('div', {'class': 'icon-with-text__children'})[2].text
-            except:
-                serves = np.nan
-            try:
-                rating = soup.find_all('span', {'class': 'sr-only'})[26].text
-            except:
-                rating = np.nan
-            try:
-                number_of_review = soup.find('span', {'class': 'rating__count-text body-copy-small'}).text
-            except:
-                number_of_review = np.nan
-            try:
-                prep_time = soup.find('li', {'class': 'body-copy-small list-item'}).text
-            except:
-                prep_time = np.nan
-            try:
-                cook_time = soup.find_all('li', {'class': 'body-copy-small list-item'})[1].text
-            except:
-                cook_time = np.nan
-            try:
-                categories = soup.find_all('ul', {
-                    'class': 'terms-icons-list d-flex post-header__term-icons-list mt-sm hidden-print list list--horizontal'})[
-                    0].text
-                if 'Vegetarian' in categories:
-                    vegetarian = 'True'
-                if not 'Vegetarian' in categories:
-                    vegetarian = False
-                if 'Vegan' in categories:
-                    vegan = True
-                if not 'Vegan' in categories:
-                    vegan = False
-                if 'Keto' in categories:
-                    keto = True
-                if not 'Keto' in categories:
-                    keto = False
-                if 'Dairy-free' in categories:
-                    dairy_free = True
-                if not 'Dairy-free' in categories:
-                    dairy_free = False
-                if 'Gluten-free' in categories:
-                    gluten_free = True
-                if not 'Gluten-free' in categories:
-                    gluten_free = False
-            except:
-                vegetarian = False
-                vegan = False
-                keto = False
-                dairy_free = False
-                gluten_free = False
-
-            i = 0
-            ingredient_list = []
-            ingredient = soup.find_all('li', {'class': 'pb-xxs pt-xxs list-item list-item--separator'})
-            while i < len(ingredient):
-                try:
-                    ingredient_string = ''.join(str(ingredient[i]).split('<!-- -->')[1])
-                except Exception as e:
-                    # print(e)
-                    ingredient_string = ''.join(ingredient[i].text)
-                    pass
-                ingredient_list.append(ingredient_string)
-                ingredient_list = [l.replace('</li>', '') for l in ingredient_list]
-                i = i + 1
-
-            print(f'Loaded recipe: {recipe_title}')
-            recipes_df = recipes_df.append(
-                {'title': recipe_title, 'difficulty': difficulty, 'serves': serves, 'rating': rating,
-                 'reviews': number_of_review, 'vegetarian': vegetarian, 'vegan': vegan, 'keto': keto,
-                 'dairy_free': dairy_free, 'gluten_free': gluten_free, 'prep_time': prep_time, 'cook_time': cook_time,
-                 'ingredient': ingredient_list}, ignore_index=True)
-
-        recipes_df = recipes_df.join(urls_df)
-
-        return recipes_df
-
-    list_urls, urls_df, recipes_df = get_urls()
-    recipes_df = get_recipes(list_urls, urls_df, recipes_df)
-    return recipes_df
-
-
-if __name__ == '__main__':
-    # enter how many pages of recipes you would like to scrape
-    pages = range_of_numbers(2)
-    # here you can change the amount of time between each request to scrape data
-    sleep_timer = 1
-    week = datetime.datetime.now().strftime("%Y-%m-%d")
-
-    print(f'Scraping {pages} pages from BBC good food')
-    recipes_df = extract(pages, sleep_timer)
-    recipes_df.to_csv(f'output/recipes_data_{week}.csv', index=False)
-    print('Complete')
+@api_view(['POST'])
+def delete_recipe(request):
+    recipe = Recipe.objects.get(id=request.data['id']).delete()
+    return Response('Recipe deleted')
